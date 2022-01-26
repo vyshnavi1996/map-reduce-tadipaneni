824649c447aa3d1ef4605013241b915df0eab9ce
vyshu <vyshu.tadipaneni14@gmail.com>
2022-01-26 14:42:43 -0600

    added all the files

---
+++ 01mapper.py
@@ -0,0 +1,12 @@
+f = open("purchase.txt","r")  # open file, read-only
+o = open("tadipaneni.txt", "w") # open file, write
+for line in f:  
+    dataList = line.strip().split("    ") 
+    print (dataList )
+    print (len(dataList ))
+    if len(dataList) == 6:
+        date, time, location, dept, amount, payType = dataList  #assign names
+        print ("{0}\t{1}".format(location, amount))
+        o.write("{0}\t{1}\n".format(location, amount))
+f.close()
+o.close()
