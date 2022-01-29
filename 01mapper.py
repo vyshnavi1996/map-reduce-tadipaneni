0a3ac58bcd1eadd1c214640aad0937ed36eca1cc
vyshu <vyshu.tadipaneni14@gmail.com>
2022-01-28 18:11:19 -0600

    added files

--- 01mapper.py
+++ 01mapper.py
@@ -1,5 +1,5 @@
 f = open("purchase.txt","r")  # open file, read-only
-o = open("tadipaneni.txt", "w") # open file, write
+o = open("tadipaneni_01.txt", "w") # open file, write
 for line in f:  
     dataList = line.strip().split("    ") 
     print (dataList )
